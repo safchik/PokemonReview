021e501ed3a2e0b59b0a81039a197db61e59e80b
Dmytro <sagchik9@gmial.com>
2023-12-19 16:17:40 +0000

    seed logic completed

--- PokemonReview/Data/DataContext.cs
+++ PokemonReview/Data/DataContext.cs
@@ -12,9 +12,9 @@ namespace PokemonReview.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Country> Countries { get; set; }
-        public  DbSet<Owner> Owners { get; set; }
+        public DbSet<Owner> Owners { get; set; }
         public DbSet<Pokemon> Pokemon { get; set; }
-        public DbSet<PokemonOwner> PokemonsOwners { get; set;}
+        public DbSet<PokemonOwner> PokemonOwners { get; set;}
         public DbSet<PokemonCategory> PokemonCategories { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Reviewer> Reviewers { get; set; }

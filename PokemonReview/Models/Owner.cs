6cc845514928938dd3a4c13ec91b62fb4df7ed38
Dmytro <sagchik9@gmial.com>
2023-12-19 12:58:12 +0000

    Add project files.

---
+++ PokemonReview/Models/Owner.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonReview.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+
+
+        public Country Country { get; set; }
+    }
+}
---
+++ PokemonReview/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonReview.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Power { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
